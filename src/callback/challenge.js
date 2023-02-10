58ec4b33543a9952fee6a5057cbb0982360682fc
Esteban Portillo <esteban.07.elias@gmail.com>
2023-02-10 16:13:09 -0600

    Fetch data - (9/26)

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -8,7 +8,7 @@ a la categoría a la cual pertenece y mostrar esta información al finalizar
 las solicitudes que estaremos haciendo a este servidor.
 */
 
-const XMLHttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.co/api/v1';
 
 function fetchData(urlApi, callback) {
@@ -19,11 +19,24 @@ function fetchData(urlApi, callback) {
     if (xhttp.readyState === 4) {
       if (xhttp.status === 200) {
         callback(null, JSON.parse(xhttp.responseText));
+      } else {
+        const error = new Error('Error' + urlApi);
+        return callback(error, null);
       }
-    } else {
-      const error = new Error('Error' + urlApi);
-      return callback(error, null);
     }
   };
   xhttp.send();
 }
+
+fetchData(`${API}/products`, function (error1, data1) {
+  if (error1) return console.error(error1);
+  fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
+    if (error2) return console.error(error2);
+    fetchData(`${API}/categories/${data2?.category?.id}`, function(error3, data3) {
+      if (error3) return console.error(error3)
+      console.log('callback1', data1[0])
+      console.log('callback2', data2.title);
+      console.log('callback3', data3.name);
+    });
+  });
+});
